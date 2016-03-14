d857b409bdc7f2b45ec0e214d22a1db70973a346
samkelo1 <samke704@gmail.com>
2016-03-14 14:00:19 +0200

    version 2

--- insert.php
+++ insert.php
@@ -1,14 +1,40 @@
 <?php
-$con = mysql_connect("localhost","root","");
-mysql_select_db("$storesurvey", $con);
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "store_survey";
 
 
- $sql = "Insert into survey(province,store,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10,surveydate)
-values  ('$_POST[ddl]','$_POST[dl2]',$_POST[sites1]''$_POST[sites2]','$_POST[sites3]','$_POST[sites4]','$_POST[sites5]','$_POST[sites6]','$_POST[sites7]','$_POST[sites8]','$_POST[sites9]''$_POST[sites10]','$_POST[surveydate]')";
-if($_POST['submit'])
-{
-echo "****Thank you****";
-mysql_query($sql);
-//this is killing my brain
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+ die("Connection failed: " . $conn->connect_error);
 }
+$province= mysqli_real_escape_string($conn,$_POST['province']) ;
+$store  =  mysqli_real_escape_string($conn,$_POST['store']) ;
+$sites1 = mysqli_real_escape_string($conn,$_POST['site1']);
+$sites2 = mysqli_real_escape_string($conn,$_POST['site2']);
+$sites3 = mysqli_real_escape_string($conn,$_POST['site3']);
+$sites4 = mysqli_real_escape_string($conn,$_POST['site4']);
+$sites5 = mysqli_real_escape_string($conn,$_POST['site5']);
+$sites6 = mysqli_real_escape_string($conn,$_POST['site6']);
+$sites7 = mysqli_real_escape_string($conn,$_POST['site7']);
+$sites8 = mysqli_real_escape_string($conn,$_POST['site8']);
+$sites9 = mysqli_real_escape_string($conn,$_POST['site9']);
+$sites10= mysqli_real_escape_string($conn,$_POST['site10']);
+
+
+ $sql = "Insert into survey(province,store,q1,q2,q3,q4,q5,q6,q7,q8,q9,q10)
+values  ('$province','$store','$sites1','$sites2','$sites3','$sites4','$sites5','$sites6','$sites7','$sites8','$sites9','$sites10')";
+
+echo $province;
+if(mysqli_query($conn, $sql)){
+ echo "Records added successfully.";
+} else{
+ echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+mysqli_close($conn);
 ?>
